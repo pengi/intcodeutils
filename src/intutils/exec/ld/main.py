8c74e20990dc3fdff27726f4f86d7b859945623f
Max Sikstrom <max@pengi.se>
2020-01-12 09:48:41 +0100

    intld: Add main command line function

--- src/intutils/exec/ld/main.py
+++ src/intutils/exec/ld/main.py
@@ -1,2 +1,34 @@
+import argparse
+from intutils import parse_intasm, parse_intelf, output_intelf, merge_intelfs
+import sys
+
+parser = argparse.ArgumentParser(description='IntCode linker')
+
+parser.add_argument(
+    'elf_files',
+    metavar='INTELF',
+    nargs='+',
+    type=argparse.FileType('r'),
+    help='Intcode elf files')
+
+parser.add_argument(
+    '-l', '--ld-file',
+    metavar='INTLD',
+    type=argparse.FileType('r'),
+    help='Linker script',
+    required=True)
+
+parser.add_argument(
+    '-o', '--out',
+    metavar='OUTPUT',
+    type=argparse.FileType('w'),
+    default=sys.stdout,
+    help='Output file. Default stdout')
+
 def main():
-    print("LD main")
+    args = parser.parse_args()
+    elfs = []
+    for elf_file in args.elf_files:
+        elfs.append(parse_intelf(elf_file))
+    output_elf = merge_intelfs(args.ld_file, elfs)
+    output_intelf(output_elf, file=args.out)
